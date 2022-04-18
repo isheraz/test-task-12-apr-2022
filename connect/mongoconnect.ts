7aa51820137b907824b045090ba351e576389605
DarabMonib <darab.monib@invozone.com>
2022-04-18 12:09:54 +0500

    Mongo URI hidden

--- connect/mongoconnect.ts
+++ connect/mongoconnect.ts
@@ -1,7 +1,7 @@
 module.exports = async function connectToMongo() {
     
     const mongoose = require('mongoose');
-    const dbConfig = process.env.MONGOURL || "mongodb+srv://hospitalapi123:hospitalapi123@cluster0.wpcbp.mongodb.net/hospitaldata?retryWrites=true&w=majority";
+    const dbConfig = process.env.MONGOURL;
     mongoose.Promise = global.Promise;
 
     mongoose.connect(dbConfig, {
